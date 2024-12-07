fcd601600aeea2594a76b007d8ff2b33affa06e8
Jace Petkau <jacepetkau@hotmail.com>
2024-12-07 16:13:32 -0500

    Stick to darker colors.

--- src/game.py
+++ src/game.py
@@ -211,4 +211,4 @@ class Game:
 
 
 def get_color() -> tuple[int, int, int]:
-    return (randint(0, 255), randint(0, 255), randint(0, 255))
+    return (randint(0, 160), randint(0, 160), randint(0, 160))
